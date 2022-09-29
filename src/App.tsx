227696e83b6328564fb722d05e6d7679c1d0dfd9
Stephan <57194608+stephan418@users.noreply.github.com>
2022-09-29 20:32:27 +0200

    Initialize  React app using create-vite

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="App">Cool, now please start the storybook instead.</div>;
+}
+
+export default App;
