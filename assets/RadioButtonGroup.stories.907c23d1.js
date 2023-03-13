f7cc2f35246fb5880b7243405ea43e7b75c698fe
stephan418 <57194608+stephan418@users.noreply.github.com>
2023-03-13 19:40:03 +0000

    Deploying to gh-pages from @ detleph/components@4adb93bbc7ebcd6dc473af3c07803503947b3ddb 🚀

---
+++ assets/RadioButtonGroup.stories.907c23d1.js
@@ -0,0 +1,28 @@
+var u=Object.defineProperty;var t=(o,r)=>u(o,"name",{value:r,configurable:!0});import{a,R as n}from"./RadioButton.12c2d38c.js";import{a as i,j as e}from"./jsx-runtime.9b32e5bc.js";import"./iframe.5dc793d5.js";const f={parameters:{storySource:{source:`import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import RadioButtonGroup from "../components/radio/group/RadioButtonGroup";
+import RadioButton from "../components/radio/button/RadioButton";
+
+export default {
+  title: "Radio button group",
+  component: RadioButtonGroup,
+  argTypes: { onSelection: { action: "select" }, value: { type: "string" } },
+} as ComponentMeta<typeof RadioButtonGroup>;
+
+const Template: ComponentStory<typeof RadioButtonGroup> = (args) => (
+  <RadioButtonGroup {...args}>
+    <RadioButton value="one" label="First option" />
+    <RadioButton value="two" label="Second option" />
+  </RadioButtonGroup>
+);
+
+export const DefaultInput = Template.bind({});
+
+DefaultInput.args = {
+  value: undefined,
+  defaultValue: "two",
+  disabled: false,
+};
+`,locationsMap:{"default-input":{startLoc:{col:58,line:13},endLoc:{col:1,line:18},startBody:{col:58,line:13},endBody:{col:1,line:18}}}}},title:"Radio button group",component:a,argTypes:{onSelection:{action:"select"},value:{type:"string"}}},p=t(o=>i(a,{...o,children:[e(n,{value:"one",label:"First option"}),e(n,{value:"two",label:"Second option"})]}),"Template"),l=p.bind({});l.args={value:void 0,defaultValue:"two",disabled:!1};const R=["DefaultInput"];export{l as DefaultInput,R as __namedExportsOrder,f as default};
+//# sourceMappingURL=RadioButtonGroup.stories.907c23d1.js.map
